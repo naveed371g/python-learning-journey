c1b1f170706092918d5f3e4a927037f164ebaad8
Your Name <your.email@example.com>
2026-05-08 00:34:18 -0700

    function demo

---
+++ fun.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+def test(a,b):
+    a=10
+    b=20
+    return (a,b)
+
+
+result = test(1,2)
+print(result)
+
+
+def fruits(a,b):
+    a = "apple"
+    b = "banana"    
+    return (a,b)
+
+print(fruits(1,2))
+
+
+
+def stock(a,b):
+   a = 7
+   b = 4
+   c = 550
+   if a > b:
+       return (a,b)
+   else:
+       return (b,a)
+
+print(stock(1,2))
+
+"""
+
+def greet (name):
+    print(f"hello my name is {name}")
+
+greet("Naveed")
+
